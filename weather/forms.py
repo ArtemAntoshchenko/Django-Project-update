1600c8f5551658839be9ab4771484c4438cb40ac
Антощенко Артем Олегович <279017030@ACADEMY.TOP>
2025-09-29 21:00:07 +0300

    add forms and views

---
+++ weather/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm, TextInput
+from .models import City, WeatherInfo
+
+class CityForm(ModelForm):
+    class Meta:
+        model = City
+        fields = ['name']
+
+class WeatherInfo(ModelForm):
+    class Meta:
+        model = WeatherInfo
+        fields = '__all__'
+        exclude = ['weatherInfo']
--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'cities'
+
+class WeatherInfo(models.Model):
+    data = models.DateTimeField()
+    weather = models.CharField(max_length=25)
+    temp = models.IntegerField()
+    weatherInfo=models.OneToOneField('WeatherInfo', on_delete=models.CASCADE, primary_key=True)
